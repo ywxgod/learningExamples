7cd9fae3081a8ffc3cf3dfcdb53d785c75117596
ywxgod <ywxgod@gmail.com>
2015-06-25 00:59:05 +0800

    add Interface class
    Signed-off-by: ywxgod <ywxgod@gmail.com>

--- jsDP/Interface.js
+++ jsDP/Interface.js
@@ -1,6 +1,9 @@
 (function(){
 	var Interface = function(name, methods){
-		
+		if(arguments.length !== 2){
+			throw new Error("Interface constructor called with " + arguments.length +
+"arguments, but expected exactly 2.");
+		}
 	};
 	
 	window.Interface = Interface;
