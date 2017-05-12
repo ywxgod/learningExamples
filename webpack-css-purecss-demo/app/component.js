d3c7e64b584ac626d100b77dc42248ff6798b3d3
wyin <ywxgod@gmail.com>
2017-05-13 00:50:36 +0800

    add webpack2 css/purecss demo

---
+++ webpack-css-purecss-demo/app/component.js
@@ -0,0 +1,8 @@
+import styles from './main.css';
+
+export default (text='Hello, world')=>{
+    const element = document.createElement('div');
+    element.innerHTML = text;
+    element.className = styles.redButton;
+    return element;
+}
