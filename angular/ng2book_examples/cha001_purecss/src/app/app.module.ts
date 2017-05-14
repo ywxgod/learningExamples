71da16a358d1726783af9d900d356c8fd2a3f187
wyin <ywxgod@gmail.com>
2017-05-15 00:49:30 +0800

    add ng2book examples - cha001 use purecss

---
+++ angular/ng2book_examples/cha001_purecss/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { UserModule } from './user/user.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import './app.css';
+
+@NgModule({
+    declarations:[AppComponent],
+    imports:[BrowserModule,UserModule],
+    providers:[],
+    bootstrap:[AppComponent]
+})
+export class AppModule{
+
+}
