51c49ffc6dfb401b9498103c7da9ade83b3321b1
wyin <ywxgod@gmail.com>
2017-08-02 11:33:31 +0800

    add lodash,jquery

--- 2017/typescript_browser/001_typescript_webpack/src/app/index.ts
+++ 2017/typescript_browser/001_typescript_webpack/src/app/index.ts
@@ -2,10 +2,20 @@ import { ObjUtil } from './utils/ObjUtil';
 import * as $ from 'jquery';
 import * as _ from 'lodash';
 
-console.log(_);
+let a1 = [1,2,3,4,5];
+let a2 = [2,3,4,8,9];
+
+//a1被删掉了哪些元素
+let r = _.difference(a1,a2);
+console.log(r);
+//a1新增了哪些元素
+let r2 = _.difference(a2, a1);
+console.log(r2);
+
+
 
 let symbol = Symbol('hoo');
-let obj = {a:1,b:2,c:3,[symbol]:5};
+let obj:{[s:string]:number} = {a:1,b:2,c:3,[symbol]:5};
 
 let app:HTMLElement = document.getElementById('app');
 //app.textContent = ObjUtil.getOwnPropertyNames(obj).toString();
