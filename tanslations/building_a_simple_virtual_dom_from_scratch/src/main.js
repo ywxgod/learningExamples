94861767f8563df2ec924d3242670aa25313d82d
ywxgod <ywxgod@gmail.com>
2019-11-08 01:50:24 +0800

    how to create virtual dom

---
+++ tanslations/building_a_simple_virtual_dom_from_scratch/src/main.js
@@ -0,0 +1,20 @@
+import createElement from "./vdom/createElement";
+import render from "./vdom/render";
+import mount from "./vdom/mount";
+
+const vApp = createElement('div', {
+    attrs: {
+        id: 'app'
+    },
+    children: [
+        'Hello world',
+        createElement('img', {
+            attrs: {
+                src: 'https://media.giphy.com/media/cuPm4p4pClZVC/giphy.gif'
+            }
+        })
+    ]
+});
+const $app = render(vApp);
+const $target = document.getElementById('app');
+mount($app, $target);
