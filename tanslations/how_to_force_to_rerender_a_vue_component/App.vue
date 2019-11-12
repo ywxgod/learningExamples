ce09640bde0115bbc41b6dccb08fff4c619d3166
ywxgod <ywxgod@gmail.com>
2019-11-12 22:29:02 +0800

    add source code for how_to_force_to_rerender_a_vue_compoennt

---
+++ tanslations/how_to_force_to_rerender_a_vue_component/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<my-component ref="child" :key="componentKey"></my-component>
+</template>
+<script>
+	import MyComponent from './MyComponent.vue';
+	export default {
+		components: { MyComponent },
+		data() {
+			return {
+				componentKey: 0
+			};
+		},
+		methods: {
+			forceRerender(){
+				this.componentKey += 1;
+			}
+		},
+		mounted(){
+			this.forceRerender();
+		}
+	};
+</script>
---
+++ tanslations/how_to_force_to_rerender_a_vue_component/MyComponent.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		Child Component
+		{{numbers.toString()}}
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		data(){
+			return {
+				numbers:[1,2,3]
+			}
+		},
+		mounted(){
+			window.console.log('mounted');
+		},
+		beforeDestroy(){
+			window.console.log('before destroyed');
+		}
+	};
+</script>
